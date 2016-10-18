a035211d9d221bd360543e23eb2b40054e0fbf58
Matthias Wiesmann <matthias.wiesmann@gmail.com>
2016-10-18 22:29:50 +0200

    off by one

--- markless.py
+++ markless.py
@@ -19,6 +19,9 @@ _box_light =  u'┌─┐│ │└─┘'
 _box_double = u'╔═╗║ ║╚═╝'
 _box_heavy =  u'┏─┓┃ ┃┗─┛'
 _box_code =   u'╭─╮┃ ┃╰─╯'
+_list_bullets = u'*+-'
+
+
 
 def _unichr(code):
   s = "\\U%08x" % code
@@ -42,6 +45,18 @@ def _makebox(in_lines, box):
 def makebox(in_lines, box):
   return '\n'.join(_makebox(in_lines, box))
 
+def makelist(in_lines, bullet):
+  indent = display_len(bullet)
+  joiner = '\n' + (' ' * indent)
+  return bullet + joiner.join(in_lines) + '\n'
+
+def is_list_start(line):
+  line = line.strip()
+  for bullet in _list_bullets:
+    if line.startswith(bullet):
+      return True
+  return False
+
 def _emphasis(text, emphasis_char):
   for char in text:
     yield char
@@ -62,22 +77,7 @@ def emphasis_runs(input, splitter, emphasis_char):
       parts.append(process_block(run))
   return ''.join(parts)
 
-def process_block(input):
-  if input.startswith('###'):
-    return makebox([process_block(input[3:].strip())], _box_light)
-  if input.startswith('##'):
-    return makebox([process_block(input[2:].strip())], _box_heavy)
-  if input.startswith('#'):
-    return makebox([process_block(input[1:].strip())], _box_double)
-  # List
-  if input.startswith('*') or input.startswith('+') or input.startswith('-'):
-    return u'•' + process_block(input[1:])
-  # Blockquote
-  if input.startswith('>>'):
-    return u'█' + process_block(input[2:])
-  if input.startswith('>'):
-    return u'▌' + process_block(input[1:])
-  # Emphasis
+def process_run(input):
   if input.find('___') >= 0:
     return emphasis_runs(input, '___', _unichr(0x0333))
   if input.find('__') >= 0:
@@ -86,38 +86,91 @@ def process_block(input):
     return emphasis_runs(input, '_', _unichr(0x20E8))
   return input
 
+def process_block(input):
+  if input.startswith('###'):
+    return makebox([process_run(input[3:].strip())], _box_light)
+  if input.startswith('##'):
+    return makebox([process_run(input[2:].strip())], _box_heavy)
+  if input.startswith('#'):
+    return makebox([process_run(input[1:].strip())], _box_double)
+  # List
+  #if is_list_start(input):
+  #  return makelist([input[1:].strip()], u'• ')
+  # Blockquote
+  if input.startswith('>>'):
+    return u'█' + process_run(input[2:])
+  if input.startswith('>'):
+    return u'▌' + process_run(input[1:])
+  return process_run(input)
+
 class Processor:
   def __init__(self, output):
     self.output = output
     self.box_lines = []
     self.text_lines = []
+    self.list_lines = []
+    self.is_list = False
 
-  def output_line(self, line):
+  def output_text(self, line):
     self.output.write(line.encode(ENCODING))
 
   def flush_text(self):
     if self.text_lines:
-      for line in self.text_lines:
-        self.output_line(process_block(line))
-      self.output_line('\n')
+      formatted_lines = map(process_run, self.text_lines)
+      self.output_text('\n'.join(formatted_lines))
+      self.output_text('\n')
       self.text_lines = []
+
+  def flush_list(self):
+    if self.list_lines:
+      formatted_lines = map(process_run, self.list_lines)
+      self.output_text(makelist(formatted_lines, u'• '))
+      self.list_lines = []
 
   def flush_box(self):
     if self.box_lines:
-      self.output_line(makebox(self.box_lines, _box_code))
+      self.output_text(makebox(self.box_lines, _box_code))
       self.box_lines = []
+
+  def flush_all(self):
+    self.flush_box()
+    self.flush_text()
+    self.flush_list()
+    self.is_list = False
 
   def process_lines(self, lines):
     for line in lines:
+      if not line:
+        self.flush_all()
+        continue
+      if line.startswith('###'):
+        self.flush_all()
+        self.output_text(makebox([process_run(line[3:].strip())], _box_light))
+        continue
+      if line.startswith('###'):
+        self.flush_all()
+        self.output_text(makebox([process_run(line[2:].strip())], _box_heavy))
+        continue
+      if line.startswith('#'):
+        self.flush_all()
+        self.output_text(makebox([process_run(line[1:].strip())], _box_double))
+        continue
       if line.startswith('    '):
         self.flush_text()
         self.box_lines.append(line[4:].rstrip())
         continue
       self.flush_box()
-      self.text_lines.append(line)
+      if is_list_start(line):
+        self.flush_all()
+        self.is_list = True
+        self.list_lines.append(line[1:].strip())
+        continue
+      if self.is_list:
+        self.list_lines.append(line.strip())
+        continue
+      self.text_lines.append(line.strip())
     # Flush everything
-    self.flush_box()
-    self.flush_text()
+    self.flush_all()
 
   def process(self, input_file):
     lines = itertools.imap(lambda l : l.decode(ENCODING), input_file)
